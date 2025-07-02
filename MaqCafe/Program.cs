8c33c93c05d7323bd2c5cece0a17db180a196d16
taykas <thaynaschaeffer@gmail.com>
2025-07-02 15:21:15 -0300

    Atualizando tudo

--- MaqCafe/Program.cs
+++ MaqCafe/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class MaquinaCafe
+{
+    public int Compartimentos { get; set; }
+    public static List<
+}
+
+public interface ISabores
+{
+    public string 
+}
+
+public class CafeLeite : ISabores
+{
+
+}
+
+public class Painel
+{
+    public int TamanhoCopo { get; set; }
+}
+
+public class Pedido
+{
+    public int TipoPedido { get; set; }
+    public 
+}
+
+public class Estoque
+{
+    public int QuantidadeCopo { get; set; }
+}
