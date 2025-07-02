680e3851be9e768def9343610f5f9a03d72f6841
taykas <thaynaschaeffer@gmail.com>
2025-07-02 13:50:26 -0300

    Montando a base do elevador

--- Elevador/Program.cs
+++ Elevador/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿// Interface que simula o funcionamento de um elevador
+
+public class Controlador
+{
+    public List<Elevador> Elevadores { get; set; } = [];
+}
+
+public interface IPainel
+{
+
+}
+
+public class Andar
+{ 
+    public int Numero { get; set; }
+    public IPainel Painel { get; set; }
+}
+
+public class Predio
+{
+    public List<Andar> Andares { get; set; } = [];
+    public List<Elevador> Elevadores { get; set; } = [];
+}
+
+public class Elevador
+{
+    public Andar Andar { get; set; }
+    public IPainel Painel { get; set; }
+    public Controlador Controlador { get; set; }
+    public int Capacidade { get; set; }
+
+}
