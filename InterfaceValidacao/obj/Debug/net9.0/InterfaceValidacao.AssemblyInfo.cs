4e3991fa5cd4d27a55d2c73de44a573ee3eba90d
taykas <thaynaschaeffer@gmail.com>
2025-07-04 13:13:28 -0300

    Atualizando

--- InterfaceValidacao/obj/Debug/net9.0/InterfaceValidacao.AssemblyInfo.cs
+++ InterfaceValidacao/obj/Debug/net9.0/InterfaceValidacao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InterfaceValidacao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca9dd77d0c7e9b4eb67dc15b99df98b0f0b68634")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b9907c770cc263fbbc092f9e457b0162b7a63f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("InterfaceValidacao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InterfaceValidacao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
