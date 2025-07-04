16660022aff8b4ec212b1ad8a637ad5bfd2c5915
taykas <thaynaschaeffer@gmail.com>
2025-07-04 14:28:28 -0300

    Finalizando explicação

---
+++ ProgAssincrona/Program.cs
@@ -0,0 +1,20 @@
+﻿while (true)
+{
+    var op = Console.ReadLine();
+    switch (op)
+    {
+        case "vernome":
+            int id = int.Parse(Console.ReadLine());
+            System.Console.WriteLine(pegarNomeUsuarioAsync(id));
+            break;
+        case "sair":
+            return;
+    }
+}
+
+string pegarNomeUsuarioAsync(int IdUsuario)
+{
+    string[] nomes = ["Don", "Cristian", "Queila"];
+    Thread.Sleep(1000);
+    return nomes[IdUsuario];
+}
