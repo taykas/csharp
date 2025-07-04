382e2f59441d80b3391ec8cf6fb380c23067ca86
taykas <thaynaschaeffer@gmail.com>
2025-07-04 14:47:02 -0300

    Começando o código

---
+++ ConexaoBD/Program.cs
@@ -0,0 +1,15 @@
+﻿// biblioteca usada dotnet add package Microsoft.EntityFrameworkCore
+// biblioteca usada p/ SQL dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+using Microsoft.EntityFrameworkCore;
+
+public class ExempleDbContext(DbContextOptions opts) : Dbcontext(opts)
+{
+
+}
+
+public class Usuario
+{
+    public int ID { get; set; }
+    public string Nome { get; set; }
+    public DateTime NascData { get; set; }
+}
