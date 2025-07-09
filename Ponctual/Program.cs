2cc7836000aca91f220ca4511cb6de6fecc0244d
taykas <thaynaschaeffer@gmail.com>
2025-07-09 15:10:38 -0300

    Novo Projeto

---
+++ Ponctual/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Formats.Tar;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+public interface ICalculadora
+{
+    float Soma(float x, float y);
+}
+
+public class Calculadora : ILogin
+    public float Login(string usuario, string senha)
+    {
+        return x + y;
+    }
+}
+
+
+public class CalculadoraIA : ICalculadora
+{
+    public float Soma(float x, float y)
+    {
+        return x + y;
+    }
+}
